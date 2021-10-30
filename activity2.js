423351de0d7d947e792453e9ed597362601424d4
yayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy <90584247+yayyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy@users.noreply.github.com>
2021-10-29 21:13:21 -0400

    Delete activity2.js

--- activity2.js
+++
@@ -1,7 +0,0 @@
-function back(){
-    window.location="activity_1.html";
-}
-function getScore(){
-    score=localStorage.getItem("score");
-    document.getElementById("update").innerHTML="<h1>score: " + score + "</h1>";
-}
